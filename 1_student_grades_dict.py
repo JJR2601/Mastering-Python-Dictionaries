6aeeed1ecf912d65a50918e4b8933bce0e4bc695
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-26 16:19:16 +0800

    refactor: fix last line

---
+++ 1_student_grades_dict.py
@@ -0,0 +1,25 @@
+student_grades = {
+    "justine": "87",
+    "july": "92",
+    "june": "85",
+    "jorge": "93",
+    "jonah": "89"
+}
+
+print(student_grades)
+
+print(student_grades.get("june"))
+
+student_grades["july"] = "91"
+
+student_grades.pop("jonah")
+
+print("jonah:", student_grades["jonah"])
+
+
+
+
+
+
+
+
