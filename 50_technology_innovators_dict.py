67d966f04ad4782ab80aa94f941f27201b8e38ca
Justine Jerald Rosel <anxespero@gmail.com>
2024-09-27 12:21:51 +0800

    Last - No change added

---
+++ 50_technology_innovators_dict.py
@@ -0,0 +1,20 @@
+technology_innovator = {
+    'Internet': 'Tim Berners Lee',
+    'Smartphone': 'Steve Jobs',
+    'Personal Computer': 'Steve Wozniak',
+    'Social Media': 'Mark Zuckerberg',
+    'Artificial Intelligence': 'John McCarthy',
+    'Blockchain': 'Satoshi Nakamoto',
+    'Virtual Reality': 'Jaron Lanier',
+    'Cloud Computing': 'Amazon Web Services Team'
+}
+
+print(technology_innovator)
+
+print(technology_innovator.get('Social Media'))
+
+technology_innovator['Smartphone'] = 'Updated Innovator: Apple Team'
+
+technology_innovator.pop('Blockchain')
+
+print('Cloud Computing:', technology_innovator['Cloud Computing'])
